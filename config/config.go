696d293c36ff0bded8ef7fa73137935bd8e410a2
falsechicken <jmdevsupport@gmail.com>
2017-04-21 16:05:23 -0400

    - Default config file is now loaded if one does not exist.

--- config/config.go
+++ config/config.go
@@ -18,10 +18,12 @@ type Config struct {
 	Console       bool
 }
 
+//Load loads the toml config file at the path provided. If the file does not exist a default one will be created at that path.
 func Load(path string) Config {
 	_, err := os.Stat(path)
 	if err != nil {
 		glogger.LogMessage(glogger.Warning, "Config file is missing: "+path)
+		generateDefaultConfig(path)
 	}
 
 	var config Config
@@ -32,5 +34,27 @@ func Load(path string) Config {
 	return config
 }
 
-func generateDefaultConfig() {
+func generateDefaultConfig(path string) {
+	defConf := new(Config)
+
+	config, err := os.Create(path)
+	if err != nil {
+		glogger.LogMessage(glogger.Error, "Cannot create config file!: "+path)
+		panic(err)
+	}
+	defer config.Close()
+
+	encoder := toml.NewEncoder(config)
+
+	defConf.Server = "example.net"
+	defConf.Username = "user@example.net"
+	defConf.Password = "superSecretSauce"
+	defConf.Status = "a"
+	defConf.StatusMessage = "GoXbot Bot"
+	defConf.StartTLS = true
+	defConf.Debug = false
+	defConf.Session = false
+	defConf.Console = false
+
+	encoder.Encode(defConf)
 }
