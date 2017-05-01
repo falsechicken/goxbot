d37d88c318f4be433f826e9d22a6a8aeb3774d53
False_Chicken <jmdevsupport@gmail.com>
2017-05-01 10:46:35 -0400

    - Got permission groups working.

--- permissions/permissions.go
+++ permissions/permissions.go
@@ -44,28 +44,56 @@ func HasPermission(jid string, permission string) bool {
 
 	glogger.LogMessage(glogger.Debug, "Checking permission "+permission+" for user "+jid)
 
-	var userFound = false
-	var userPerms *[]string
-	for _, user := range permsTable.Users {
-		if user.JID == jid {
-			userFound = true
-			userPerms = &user.Permissions
-		}
-		if userFound == true {
-			break
-		}
-	}
-
-	if userFound {
-		for _, perm := range *userPerms {
-			if perm == permission {
+	if exists, user := getUser(jid); exists {
+		for _, p := range user.Permissions {
+			if p == permission {
 				return true
 			}
 		}
-		return false
-	} else {
-		return false
+		_, g := GetGroups(jid)
+
+		for _, gN := range g {
+			if checkGroupPerms(gN, permission) {
+				return true
+			}
+
+		}
+
 	}
+	return false
+}
+
+//IsGroupMember returns true is a user is in the provided group. Will also return false if the group does not exist.
+func IsGroupMember(jid string, group string) bool {
+	jid = strings.Split(jid, "/")[0]
+	if exists, groups := GetGroups(jid); exists {
+		for _, g := range groups {
+			if g == group {
+				return true
+			}
+		}
+	}
+	return false
+
+}
+
+func GetGroups(jid string) (bool, []string) {
+	exists, user := getUser(jid)
+	if exists {
+		g := make([]string, len(user.Groups))
+		copy(g, user.Groups)
+		return true, g
+	}
+	return false, make([]string, 1)
+}
+
+func getUser(jid string) (bool, User) {
+	for i, u := range permsTable.Users {
+		if u.JID == jid {
+			return true, permsTable.Users[i]
+		}
+	}
+	return false, *new(User)
 }
 
 func GrantPermission(jid string, permission string) {
@@ -119,7 +147,7 @@ func generateDefaultConfig(path string) bool {
 	glogger.LogMessage(glogger.Info, "Generating default permissions file...")
 
 	var defaultPerms = new(PermsTable)
-	defaultPerms.Users = make([]User, 2, 2)
+	defaultPerms.Users = make([]User, 2)
 
 	defaultPerms.Users = append(defaultPerms.Users, *new(User), *new(User))
 	defaultPerms.Users[0].JID = "user@example.net"
@@ -156,17 +184,17 @@ func generateDefaultGroups(path string) bool {
 	glogger.LogMessage(glogger.Info, "Generating default groups file...")
 
 	var defaultGroups = new(GroupsTable)
-	defaultGroups.Groups = make([]PermGroup, 1, 1)
+	defaultGroups.Groups = make([]PermGroup, 1)
 
 	defaultGroups.Groups = append(defaultGroups.Groups, *new(PermGroup), *new(PermGroup))
 
 	defaultGroups.Groups[0].Name = "default"
-	defaultGroups.Groups[0].Permissions = make([]string, 2, 2)
+	defaultGroups.Groups[0].Permissions = make([]string, 2)
 	defaultGroups.Groups[0].Permissions[0] = "talk"
 	defaultGroups.Groups[0].Permissions[1] = "status"
 
 	defaultGroups.Groups[1].Name = "user"
-	defaultGroups.Groups[1].Permissions = make([]string, 3, 3)
+	defaultGroups.Groups[1].Permissions = make([]string, 3)
 	defaultGroups.Groups[1].Permissions[0] = "shout"
 	defaultGroups.Groups[1].Permissions[1] = "move"
 	defaultGroups.Groups[1].Permissions[2] = "poke"
@@ -182,4 +210,19 @@ func generateDefaultGroups(path string) bool {
 	encoder.Encode(defaultGroups)
 
 	return true
+}
+
+//checkGroupPerms returns true if the group contains the provided permission.
+func checkGroupPerms(group string, perm string) bool {
+	for _, g := range groupsTable.Groups {
+		if g.Name == group {
+			for _, p := range g.Permissions {
+				if p == perm {
+					return true
+				}
+			}
+			return false
+		}
+	}
+	return false
 }
